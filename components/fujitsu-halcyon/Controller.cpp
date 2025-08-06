738d595e45d4cf6acd9a52c7dd20db0d72cfb4b3
AusGadgets <chris.davidson@cantrayit.com.au>
2025-08-06 12:25:05 +0800

    Update Controller.cpp

--- components/fujitsu-halcyon/Controller.cpp
+++ components/fujitsu-halcyon/Controller.cpp
@@ -229,12 +229,9 @@ void Controller::process_packet(const Packet::Buffer& buffer, bool lastPacketOnW
         if (this->initialization_stage == InitializationStageEnum::FeatureRequest)
             tx_packet.Type = PacketTypeEnum::Features;
         else if ((error_flag_changed && this->is_primary_controller()) ||
-                 (packet.Type == PacketTypeEnum::Error && !this->is_primary_controller()))
+                 (packet.Type == PacketTypeEnum::Error && !this->is_primary_controller())) {
             tx_packet.Type = PacketTypeEnum::Error;
-        else if (!this->function_queue.empty()) {
-            tx_packet.Type = PacketTypeEnum::Function;
-            tx_packet.Function = this->function_queue.front();
-            this->function_queue.pop();
+            tx_packet.Error.ErrorCode = 1; // Generic error code
         }
         else {
             // First CONFIG packet sent from Fujitsu controller has write flag set, but we do not restore state at this time
